8dbebbdf5943f45fa26cdd67ae351404ead5b065
Carlos Ramos <rammen.carlos@gmail.com>
2021-10-04 16:41:48 -0500

    p1

---
+++ PServidor/Proyectos/P2.MVC/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P2.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P2.MVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            UserViewModel userVM = new UserViewModel();
+            userVM.Id = 1;
+            userVM.Name = "Carlos";
+            userVM.IsEnabled = true;
+
+            return View(userVM);
+        }
+
+
+    }
+}
---
+++ PServidor/Proyectos/P2.MVC/Models/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P2.MVC.Models
+{
+    public class UserViewModel
+    {
+        public Int32 Id { get; set; }
+        public string Name { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+}
