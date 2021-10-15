4c2d07408570f221911cac0ef475197096ecea83
Carlos Ramos <rammen.carlos@gmail.com>
2021-10-15 16:57:13 -0500

    user

---
+++ PServidor/Proyectos/P3.CRUD/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P3.CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P3.CRUD.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<UserViewModel> users = new List<UserViewModel>();
+            
+            UserViewModel userModel = new UserViewModel();
+            userModel.Id = 1;
+            userModel.UserName = "carlos.ramos";
+            userModel.Name = "Carlos Ramos";
+            userModel.Password = "1234";
+            userModel.CreatedDate = DateTime.Now;
+            users.Add(userModel);
+
+            return View(users);
+        }
+
+        public IActionResult AddOrEdit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult FormAddOrEdit([FromForm] UserViewModel user)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ PServidor/Proyectos/Utilities.Persistence/UserRespository.cs
@@ -0,0 +1,16 @@
+﻿using P3.CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Persistence
+{
+
+    public class UserRespository
+    {
+        public List<UserViewModel> _users;
+
+    }
+}
